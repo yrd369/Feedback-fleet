2446344d3c0ce10486dc2435b748ed7abf068731
Riyaz <91373561+Riyaz1000@users.noreply.github.com>
2024-02-26 12:04:31 +0530

    createAboutPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import TheAbout from "./components/About";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="font-poppins ">
       <Navbar />
+      <TheAbout />
     </div>
   );
 }
---
+++ client/src/components/About.jsx
@@ -0,0 +1,13 @@
+const TheAbout = () => {
+  return (
+    <div className="min-h-screen bg-gray-400">
+      <p className="bg-red-300 text-black p-5 font-poppins font-semibold text-3xl">
+        Cyberdude Website
+      </p>
+
+      <p></p>
+    </div>
+  );
+};
+
+export default TheAbout;
